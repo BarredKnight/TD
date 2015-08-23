e068cba71362fdd1eb39f6d6faa7741c5cf4785c
Andrew Zhurov <barredknight@gmail.com>
2015-08-24 02:57:24 +0300

    Word get fixed

--- src/com/td/model/Word.java
+++ src/com/td/model/Word.java
@@ -1,6 +1,6 @@
 package com.td.model;
 
-import com.td.model.Words;
+import com.td.model.parameters.WordStatus;
 import com.td.model.parameters.WordType;
 import com.td.model.parameters.WordSense;
 import com.td.model.parameters.WordPronunciation;
@@ -17,11 +17,14 @@ public class Word {
 
 	public final Words links;
 
-	public Word(final WordType type, final WordSense sense, final String spelling, final WordPronunciation pronunciation, final Words links) {
+	public final WordStatus status;
+
+	public Word(final WordType type, final WordSense sense, final String spelling, final WordPronunciation pronunciation, final Words links, final WordStatus status) {
 		this.type = type;
 		this.sense = sense;
 		this.spelling = spelling;
 		this.pronunciation = pronunciation;
 		this.links = links;
+		this.status = status;
 	}
 }
