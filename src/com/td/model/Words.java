47cef70d1414fb6bf0a31009300264394d94fa6d
Andrew Zhurov <barredknight@gmail.com>
2015-08-29 20:48:05 +0300

    Words completed. But ugly.

--- src/com/td/model/Words.java
+++ src/com/td/model/Words.java
@@ -1,8 +1,10 @@
 package com.td.model;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Words<O,M> {
+public class Words implements Iterator{
 
 	public final String sortedBy;
 
@@ -10,9 +12,24 @@ public class Words<O,M> {
 
 	public final Path location;
 
-	public Words(final String sortedBy, final Word language, final Path location) {
+	public final ArrayList<Word> massive;
+
+	public int index = 0;
+
+	public Words(final String sortedBy, final Word language, final Path location, final ArrayList<Word> massive) {
 		this.sortedBy = sortedBy;
 		this.language = language;
 		this.location = location;
+		this.massive = massive;
+	}
+
+	@Override
+	public boolean hasNext() { ////////////////////////////////////////////////// Maybe error
+		return massive.get(index) != null;
+	}
+
+	@Override
+	public Object next() {
+		return massive.get(index);
 	}
 }
