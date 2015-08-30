75751a15a131e1519b2eca43a74262a1d88d874c
Andrew Zhurov <barredknight@gmail.com>
2015-08-30 04:22:32 +0300

    ConsoleView is updated. It's pretty good

--- src/com/td/view/ConsoleView.java
+++ src/com/td/view/ConsoleView.java
@@ -1,5 +1,22 @@
 package com.td.view;
 
+import com.td.model.Word;
+import com.td.model.Words;
+
 public class ConsoleView {
+
+    public static void showWords(Words words){
+        boolean ln = false;
+        for (Word word : words.massive){
+
+            if (ln)
+                System.out.println(word.spelling);
+            else{
+                System.out.print("      ");
+                System.out.print(word.spelling);
+            }
+            ln = !ln;
+        }
+    }
 
 }
