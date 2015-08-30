3bb5ee7fd7e01497927da21fcab4e5d3552f1ac8
Andrew Zhurov <barredknight@gmail.com>
2015-08-30 04:23:00 +0300

    WordsTranslator updated. Still looks good

--- src/com/td/controller/WordsTranslator.java
+++ src/com/td/controller/WordsTranslator.java
@@ -7,7 +7,7 @@ import com.td.model.Words;
 
 public class WordsTranslator {
 
-	public Words translate(Words words) {
+	public static Words translate(Words words) {
 		for (int i = 0; i < words.massive.size(); i++){
 			words.massive.add(i+1, translate(words.massive.get(i)));
 			i = i+1;
@@ -15,7 +15,7 @@ public class WordsTranslator {
 		return  words;
 	}
 
-	private Word translate(Word word){
+	private static Word translate(Word word){
 		Translator translator = Translator.getInstance();
 		return new Word(translator.translate(word.spelling, Language.ENGLISH, Language.RUSSIAN));
 	}
