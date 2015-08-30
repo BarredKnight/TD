74eb4d71e4fed09f37d46515662c749f0ac4b192
Andrew Zhurov <barredknight@gmail.com>
2015-08-30 05:27:45 +0300

    Loader changed, because of unexpected problem in external library

--- src/com/td/controller/Loader.java
+++ src/com/td/controller/Loader.java
@@ -13,11 +13,10 @@ import java.io.IOException;
 public class Loader {
 
 	final FileReader fileReader;
-	final File file;
 
-	public Loader(File file) throws FileNotFoundException {
-		fileReader = new FileReader(file);
-		this.file = file;
+	public Loader(String path) throws FileNotFoundException {
+		fileReader = new FileReader(path);
+
 	}
 
 	public Text loadText() throws IOException {
@@ -27,9 +26,9 @@ public class Loader {
 			fileReader.read(chars);
 			string = string + chars;
 			}
-		Translator translate = Translator.getInstance();
-		String language = translate.detect(string.substring(0, 15));
-		final Text text = new Text(string, new Word(language), file.toPath());
+//		Translator translate = Translator.getInstance();
+//		String language = translate.detect(string.substring(0, 5));		WEIRD error, really
+		final Text text = new Text(string, new Word(Language.ENGLISH), null);
 		return text;
 		}
 	}
