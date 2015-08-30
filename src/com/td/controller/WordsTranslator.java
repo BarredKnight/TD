222ab3e56606c2eb4b15c2dbb3350215d2059d80
Andrew Zhurov <barredknight@gmail.com>
2015-08-30 03:26:42 +0300

    WordsTranslator is created. Looks good

--- src/com/td/controller/WordsTranslator.java
+++ src/com/td/controller/WordsTranslator.java
@@ -1,11 +1,23 @@
 package com.td.controller;
 
+import com.gtranslate.Language;
+import com.gtranslate.Translator;
+import com.td.model.Word;
 import com.td.model.Words;
 
 public class WordsTranslator {
 
 	public Words translate(Words words) {
-		return null;
+		for (int i = 0; i < words.massive.size(); i++){
+			words.massive.add(i+1, translate(words.massive.get(i)));
+			i = i+1;
+		}
+		return  words;
+	}
+
+	private Word translate(Word word){
+		Translator translator = Translator.getInstance();
+		return new Word(translator.translate(word.spelling, Language.ENGLISH, Language.RUSSIAN));
 	}
 
 }
