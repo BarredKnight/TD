aee8b98a1da8e4b5f94a1f9230752cab4a772416
Andrew Zhurov <barredknight@gmail.com>
2015-09-01 02:26:56 +0300

    ConsoleView now work right. Shows correct

--- src/com/td/view/ConsoleView.java
+++ src/com/td/view/ConsoleView.java
@@ -5,6 +5,8 @@ import com.td.model.Words;
 
 public class ConsoleView {
 
+    private static final int CELL_LENGTH = 15;
+
     public static void showWords(Words words){
         boolean ln = false;
         for (Word word : words.massive){
@@ -12,8 +14,10 @@ public class ConsoleView {
             if (ln)
                 System.out.println(word.spelling);
             else{
-                System.out.print("      ");
                 System.out.print(word.spelling);
+                for (int i = 0; i < CELL_LENGTH - word.spelling.length(); i++){
+                    System.out.print(" ");
+                }
             }
             ln = !ln;
         }
