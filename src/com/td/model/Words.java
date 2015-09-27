f902fd6186899bbbc5458f454a965da7df3ff120
Andrew Zhurov <barredknight@gmail.com>
2015-09-27 21:12:20 +0300

    class Words corrected

--- src/com/td/model/Words.java
+++ src/com/td/model/Words.java
@@ -2,19 +2,15 @@ package com.td.model;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 
-public class Words implements Iterator{
+public class Words{
 
 	public final String sortedBy;
 
 	public final Path location;
 
 	public final LinkedHashMap <Word,ArrayList<Word>> ourWords;
-
-	public int index = 0;
-
 
 
 	public Words(final String sortedBy, final Path location, final LinkedHashMap <Word,ArrayList<Word>> ourWords) {
@@ -23,15 +19,9 @@ public class Words implements Iterator{
 		this.ourWords = ourWords;
 	}
 
-
-
-	@Override
-	public boolean hasNext() { ////////////////////////////////////////////////// Maybe error
-		return ourWords.get(index) != null;
-	}
-
-	@Override
-	public Object next() {
-		return ourWords.get(index);
+	public Words(LinkedHashMap<Word, ArrayList<Word>> ourWords) {
+		this.ourWords = ourWords;
+		this.sortedBy = null;
+		this.location = null;
 	}
 }
