fe9a7369b9003243079cd5d5b6a155cb0ee08f7d
Rende11 <artamonovn@gmail.com>
2015-09-27 19:23:55 +0300

    created LinkedHashMap

--- src/com/td/controller/WordsTranslator.java
+++ src/com/td/controller/WordsTranslator.java
@@ -5,13 +5,28 @@ import com.gtranslate.Translator;
 import com.td.model.Word;
 import com.td.model.Words;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+
 public class WordsTranslator {
 
 	public static Words translate(Words words) {
-		for (int i = 0; i < words.massive.size(); i++){
-			words.massive.add(i+1, translate(words.massive.get(i)));
-			i = i+1;
+
+		Iterator<Map.Entry<Word,ArrayList<Word>>> itr = words.ourWords.entrySet().iterator();
+
+		while (itr.hasNext()){
+
+			Map.Entry<Word,ArrayList<Word>> entry = itr.next();
+
+			ArrayList<Word> trans = new ArrayList<>();
+			trans.add(translate(entry.getKey()));
+
+
+			words.ourWords.put(entry.getKey(),trans);
+
 		}
+
 		return  words;
 	}
 
