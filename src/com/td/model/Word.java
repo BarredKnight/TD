1916ad352bad46622cca2051ae57bf7f9d579e0b
Andrew Zhurov <barredknight@gmail.com>
2015-09-30 16:40:22 +0300

    Word changed. Now comparing is going by using our "spelling" field. If it compare - then our objects identical

--- src/com/td/model/Word.java
+++ src/com/td/model/Word.java
@@ -8,7 +8,26 @@ public class Word {
 		this.spelling = spelling;
 	}
 
+	@Override
 	public String toString(){
 		return spelling;
 	}
+
+	@Override
+	public int hashCode() {
+		return spelling.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Word word = (Word) o;
+
+		return spelling.equals(word.spelling);
+
+	}
+
+
 }
