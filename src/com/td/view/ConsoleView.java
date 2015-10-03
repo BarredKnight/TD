6b0819797cde4b823f9bfde196b5fcdf64ff63bb
Andrew Zhurov <barredknight@gmail.com>
2015-10-04 00:06:37 +0300

    ConsoleView updated. Added loop over path to file setting

--- src/com/td/view/ConsoleView.java
+++ src/com/td/view/ConsoleView.java
@@ -17,24 +17,12 @@ public class ConsoleView {
 
     private final static String BORDER = "---";
 
-    public static Text load(){
-
-        final Scanner pathScanner = new Scanner(System.in);
-
+    public static Text load() {
         System.out.println(BORDER + "loading" + BORDER);
-        System.out.println("Enter path to text-file :");
-        try {
-            Loader loader = new Loader(pathScanner.nextLine());
-            return loader.loadText();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return loadText();
     }
 
-    public static Words crush(Text clearText){
+    public static Words crush(Text clearText) {
         System.out.println(BORDER + "crushing" + BORDER);
         try {
             return Crusher.crush(clearText);
@@ -44,14 +32,15 @@ public class ConsoleView {
         return null;
     }
 
-    public static Words translate(Words clearWords){
+    public static Words translate(Words clearWords) {
         System.out.println(BORDER + "translating" + BORDER);
         return WordsTranslator.translate(clearWords);
     }
 
-    public static void sort(){}
+    public static void sort() {
+    }
 
-    public static void holdOn(int ms){
+    public static void holdOn(int ms) {
         System.out.println(BORDER + "don't hurry" + BORDER);
         try {
             Thread.sleep(ms);
@@ -60,32 +49,49 @@ public class ConsoleView {
         }
     }
 
-    public static void show(Words words){
+    public static void show(Words words) {
 
         System.out.println(BORDER + "showing" + BORDER);
         holdOn(2000);
-        for (String stringToPrint : getLinesToShow(words.ourWords.toString())){
+        for (String stringToPrint : getLinesToShow(words.ourWords.toString())) {
             System.out.println(stringToPrint);
         }
-     }
+    }
 
     /**
      * Works only for string, gotten by toString from Map
+     *
      * @return the {@code List} of string, gotten from input String
-     * */
-    private static List<String> getLinesToShow(final String lineToSeparate){
+     */
+    private static List<String> getLinesToShow(final String lineToSeparate) {
 
         int newLineFrom = 1;
         List<String> outputLines = new ArrayList<>();
 
-        for (int i = 0; i < lineToSeparate.length(); i++){
-            if (lineToSeparate.substring(i, i+1).equals("]")){
-                outputLines.add(lineToSeparate.substring(newLineFrom, i+1));
-                newLineFrom = i+3;
+        for (int i = 0; i < lineToSeparate.length(); i++) {
+            if (lineToSeparate.substring(i, i + 1).equals("]")) {
+                outputLines.add(lineToSeparate.substring(newLineFrom, i + 1));
+                newLineFrom = i + 3;
             }
         }
 
         return outputLines;
     }
+
+    private static Text loadText() {
+        final Scanner pathScanner = new Scanner(System.in);
+
+        System.out.println("Enter path to text-file :");
+        while (true) {
+            try {
+                Loader loader = new Loader(pathScanner.nextLine());
+                return loader.loadText();
+            } catch (FileNotFoundException e) {
+                System.out.println("Wrong way! Try again:");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
+}
