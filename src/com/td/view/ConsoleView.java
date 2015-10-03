dc25b4cb1ea96a78fe5d5cf128f3a4e66e2a9c9c
Andrew Zhurov <barredknight@gmail.com>
2015-10-03 23:51:02 +0300

    ConsoleView. Made.

--- src/com/td/view/ConsoleView.java
+++ src/com/td/view/ConsoleView.java
@@ -1,15 +1,69 @@
 package com.td.view;
 
+import com.td.controller.Crusher;
+import com.td.controller.Loader;
+import com.td.controller.WordsTranslator;
+import com.td.model.Text;
 import com.td.model.Words;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ConsoleView {
 
+    private final static String BORDER = "---";
 
-    public static void showWords(Words words){
+    public static Text load(){
 
+        final Scanner pathScanner = new Scanner(System.in);
+
+        System.out.println(BORDER + "loading" + BORDER);
+        System.out.println("Enter path to text-file :");
+        try {
+            Loader loader = new Loader(pathScanner.nextLine());
+            return loader.loadText();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Words crush(Text clearText){
+        System.out.println(BORDER + "crushing" + BORDER);
+        try {
+            return Crusher.crush(clearText);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Words translate(Words clearWords){
+        System.out.println(BORDER + "translating" + BORDER);
+        return WordsTranslator.translate(clearWords);
+    }
+
+    public static void sort(){}
+
+    public static void holdOn(int ms){
+        System.out.println(BORDER + "don't hurry" + BORDER);
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void show(Words words){
+
+        System.out.println(BORDER + "showing" + BORDER);
+        holdOn(2000);
         for (String stringToPrint : getLinesToShow(words.ourWords.toString())){
             System.out.println(stringToPrint);
         }
@@ -22,15 +76,12 @@ public class ConsoleView {
     private static List<String> getLinesToShow(final String lineToSeparate){
 
         int newLineFrom = 1;
-        List<String> outputLines = new ArrayList<String>();
+        List<String> outputLines = new ArrayList<>();
 
         for (int i = 0; i < lineToSeparate.length(); i++){
             if (lineToSeparate.substring(i, i+1).equals("]")){
                 outputLines.add(lineToSeparate.substring(newLineFrom, i+1));
                 newLineFrom = i+3;
-            }
-            else{
-
             }
         }
 
