39943ce349e31ebc537571474028c8099fb69da5
Rende11 <artamonovn@gmail.com>
2015-10-15 22:30:26 +0300

    refactor translate method

--- src/com/td/controller/WordsTranslator.java
+++ src/com/td/controller/WordsTranslator.java
@@ -6,25 +6,17 @@ import com.td.model.Word;
 import com.td.model.Words;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Map;
+
 
 public class WordsTranslator {
 
 	public static Words translate(Words words) {
 
-		Iterator<Map.Entry<Word,ArrayList<Word>>> itr = words.ourWords.entrySet().iterator();
+	for (Word word : words.ourWords.keySet()){
 
-		while (itr.hasNext()){
-
-			Map.Entry<Word,ArrayList<Word>> entry = itr.next();
-
-			ArrayList<Word> trans = new ArrayList<>();
-			trans.add(translate(entry.getKey()));
-
-
-			words.ourWords.put(entry.getKey(),trans);
-
+			ArrayList<Word> translateWord = new ArrayList<>();
+			translateWord.add(translate(word));
+			words.ourWords.put(word,translateWord);
 		}
 
 		return  words;
